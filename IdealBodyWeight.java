6d6b11ad4baf032a140ec0a051f3f7deec7e67bc
Kate Portalatin <k8portalatin@gmail.com>
2014-07-06 18:38:58 -0400

    Update IdealBodyWeight.java
    Loop added to read info from database file.

--- IdealBodyWeight.java
+++ IdealBodyWeight.java
@@ -23,6 +23,7 @@ public class IdealBodyWeight {
         int feet, inches;  // for a person's height
         Scanner tenneile = new Scanner(System.in);
         Scanner captain = new Scanner(System.in);
+        String clear;
         
         // gather user info
         System.out.println("Please enter your height in first in feet and then "
@@ -38,26 +39,16 @@ public class IdealBodyWeight {
         System.out.println("Now let's gather info from the database file.");
         try {
             captain = new Scanner(new FileInputStream("heights.txt"));
-            name = captain.nextLine();
-            feet = captain.nextInt();
-            inches = captain.nextInt();
-            ideal = 110 + ((feet - 5) * 110) + (inches * 5);
-            System.out.printf("%s's ideal body weight at %d feet and "
-                        + "%d inches is %d.%n", name, feet, inches, ideal);
-            String clear = captain.nextLine();
-            name = captain.nextLine();
-            feet = captain.nextInt();
-            inches = captain.nextInt();
-            ideal = 110 + ((feet - 5) * 110) + (inches * 5);
-            System.out.printf("%s's ideal body weight at %d feet and "
-                        + "%d inches is %d.%n", name, feet, inches, ideal);  
-            clear = captain.nextLine();
-            name = captain.nextLine();
-            feet = captain.nextInt();
-            inches = captain.nextInt();
-            ideal = 110 + ((feet - 5) * 110) + (inches * 5);
-            System.out.printf("%s's ideal body weight at %d feet and "
-                        + "%d inches is %d.%n", name, feet, inches, ideal);  
+            while (captain.hasNext()) {
+               
+                name = captain.nextLine();
+                feet = captain.nextInt();
+                inches = captain.nextInt();
+                ideal = 110 + ((feet - 5) * 110) + (inches * 5);
+                System.out.printf("%s's ideal body weight at %d feet and "
+                            + "%d inches is %d.%n", name, feet, inches, ideal);
+                clear = captain.nextLine();
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             System.exit(0);

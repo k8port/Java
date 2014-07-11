cd2af2eab484a6838fb0be81db2b964f9a2a2924
Kate Portalatin <k8portalatin@gmail.com>
2014-07-11 12:21:03 -0400

    Update Fraction.java
    Added main method to run as program and for test and debug purposes. Added set method that allows user to set numerator and denominator at same time. Added get method that allows user to retrieve fraction expressed as a percentage. Changed equals method in order to test for equality of value not just for matching numerators and denominators.

--- Fraction.java
+++ Fraction.java
@@ -1,12 +1,19 @@
 /** Class used to represent a ration of two integers. Includes a mutator method
- *  that allows user to set the numerator and denominator. Includes a method
- *  that returns the value of numerator divided by denominator. Includes a 
- *  method that outputs the value of the fraction in its lowest terms (most
- *  simplified).  Class that uses object called "MakeFractions.java".
+ *  that allows user to set the numerator and denominator both separately and
+ *  together. Includes a method that returns the value of numerator divided by 
+ *  denominator. Includes a method that outputs the value of the fraction in its 
+ *  lowest terms (most simplified).  Equals method evaluates Fractions for equality
+ *  in value. Main method allows user to create a Fraction and then loops 
+ *  repeatedly until user wants to quit. Program allows user to enter a target
+ *  fraction into an anonymous object and learn whether fractions are identical
+ *  in value.
  */
 
 package fraction;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 /**
  *
  * @author k8port
@@ -28,6 +35,7 @@ public class Fraction {
     
     /** Constructor that allows user to set numerator.
      *  Sets denominator to 1.
+     *  @param numerator the top half of the fraction
      */
     public Fraction(int numerator) {
         this.numerator = numerator;
@@ -35,7 +43,8 @@ public class Fraction {
     }
     
     /** Constructor that allows user to set numerator and denominator.
-     * 
+     *  @param numerator the top half of Fraction
+     *  @param denominator the bottom half of Fraction
      */
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
@@ -68,6 +77,23 @@ public class Fraction {
             this.denominator = denominator;
     }
     
+    /**
+     *  Method that allows user to set both numerator and denominator.
+     *  Must be positive whole numbers.
+     *  @param num the top half of Fraction
+     *  @param den the bottom half of Fraction
+     */
+    public void setFraction(int num, int den) {
+        if (num < 1 || den < 1) {
+            System.out.println("Fatal error.");
+            System.exit(1);
+        }
+        else {
+            numerator = num;
+            denominator = den;
+        }    
+    }
+    
     /** Method to access private variable.   
      * @return the numerator
      */
@@ -77,6 +103,15 @@ public class Fraction {
      * @return the denominator
      */
     public int getDenominator() {return denominator;}
+    
+    /** 
+     *  Method to return Fraction expressed as numerator divided by denominator.
+     *  @return the numerator divided by the denominator expressed as String percentage. 
+     */
+    public String getPercent()  {
+        DecimalFormat decimalFormatter = new DecimalFormat("#0.0#%");
+        return decimalFormatter.format((double)numerator/(double)denominator);
+    }
     
     // other methods
     /** Method to return the largest common factor of the calling Fraction object.
@@ -110,15 +145,20 @@ public class Fraction {
         return fraction;
     }
     
-    /** Method to evaluate if fraction is equal to calling object.
-     *  @pre The calling object and the parameter object have been reduced to 
-     *          lowest terms,
-     *  @param otherFraction The fraction we are evaluating against this
+    /** Method to evaluate if parameter fraction is equal to calling object.
+     *  Evaluates both fractions as if they are in lowest terms.
+     *  @param otherFraction The fraction we are evaluating against calling Fraction
      *  @return boolean true if equal
      */
     public boolean equals(Fraction otherFraction) {
-        return (numerator == otherFraction.getNumerator()) &&
-                (otherFraction.getDenominator() == denominator);
+        if (otherFraction == null) 
+            return false;
+        else {
+            Fraction calling = this.getLowestTerms();
+            Fraction param = otherFraction.getLowestTerms();
+            return (calling.getNumerator() == param.getNumerator()) &&
+                (calling.getDenominator() == param.getDenominator());
+        }
     }
     
     /** Method to return String representation of Fraction object.
@@ -127,5 +167,43 @@ public class Fraction {
     @Override
     public String toString() {
         return this.getNumerator() + "/" + this.getDenominator();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        StringTokenizer stringTokenizer;
+        Scanner scanner = new Scanner(System.in);
+        boolean enter; // true if continue entering Fractions
+        String temp; // to hold user input for evaluation
+        Fraction a; // the current fraction being evaluated
+        
+        do {            
+            System.out.println("Enter a Fraction:");
+            temp = scanner.nextLine();
+            stringTokenizer = new StringTokenizer(temp, ",/ ");
+            // construct the Fraction
+            a = new Fraction(Integer.valueOf(stringTokenizer.nextToken()), Integer.valueOf(stringTokenizer.nextToken()));
+            System.out.println("You entered " + a.toString());
+            System.out.println("In its lowest terms: " + a.getLowestTerms());
+            System.out.println("Now reset the denominator.");
+            temp = scanner.nextLine();
+            a.setDenominator(Integer.valueOf(temp));
+            System.out.println("Fraction is now " + a);
+            System.out.println("As a percentage it is " + a.getPercent());
+            System.out.println("In lowest terms it is " + a.getLowestTerms());
+            System.out.println("Now lets test for equality.");
+            System.out.println("Enter another fraction to compare:");
+            temp = scanner.nextLine();
+            stringTokenizer = new StringTokenizer(temp, ",/ ");
+            System.out.println("Equal? " + a.equals(new Fraction(Integer.valueOf
+                                (stringTokenizer.nextToken()), Integer.valueOf
+                                (stringTokenizer.nextToken()))));
+            System.out.println("Would you like to keep using program? True or False.");
+            enter = scanner.nextBoolean();
+            temp = scanner.nextLine(); // clear
+        } while (enter);
     }
 }
